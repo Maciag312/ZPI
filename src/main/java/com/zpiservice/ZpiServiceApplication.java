3f7c133321095476ab01d9e6a0d8ea506cd3f23b
Bartłomiej Chmiel <bachm44@gmail.com>
2021-07-08 11:44:34 +0200

    Generated project

---
+++ src/main/java/com/zpiservice/ZpiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zpiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZpiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zpiservice/ZpiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
