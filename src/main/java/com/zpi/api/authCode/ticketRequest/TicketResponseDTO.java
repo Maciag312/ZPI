4b7df01898e5a3e86c8aced483d347a6d24cef6a
Bartłomiej Chmiel <bachm44@gmail.com>
2021-11-29 16:59:42 +0100

    Return hardcoded scope

--- src/main/java/com/zpi/api/authCode/ticketRequest/TicketResponseDTO.java
+++ src/main/java/com/zpi/api/authCode/ticketRequest/TicketResponseDTO.java
@@ -8,10 +8,12 @@ public class TicketResponseDTO {
     private final String ticket;
     private final String ticket_type;
     private final String state;
+    private final String scope;
 
     public TicketResponseDTO(TicketResponse response) {
         this.ticket = response.getTicket();
         this.ticket_type = response.getTicketType().getName();
         this.state = response.getState();
+        this.scope = "profile";
     }
 }
